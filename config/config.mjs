ea6e56f53b1e43ac2c5d066c83697863a4f141c4
Arif <arif.hossain.9213@gmail.com>
2024-12-30 14:58:04 +0600

    fix

--- config/config.mjs
+++ config/config.mjs
@@ -1,26 +1,22 @@
 // js code
-import { colors } from './core/vars.js';
-import { downloadFile, handleAdd } from './core/functions.js'
-
-import BuildTool from './build_tool/build_tool.js'
+import { build, unbuild } from './build_tool/build_tool.js';
+import { handleAdd } from './helper/add/handle_add.js';
 
 const scriptDir = import.meta.dirname;
 
 // CLI interface
 const [, , command] = process.argv;
-const buildTool = new BuildTool();
 
 switch (command) {
   case 'build':
-    await buildTool.build();
+    await build();
     break;
   case 'unbuild':
-    await buildTool.unbuild();
+    await unbuild();
     break;
   case 'add':
     await handleAdd(scriptDir);
     break;
   default:
-    console.log(`${colors.style('config.mjs:', colors.fg.cyan)} Can not handle "${command}", supported command are ${colors.style('[build|unbuild|add]', colors.fg.yellow)}.`);
     process.exit(1);
 }

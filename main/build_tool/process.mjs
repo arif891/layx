b7d90da440cdd50f04607fabbf740c7d407596a2
Arif <arif.hossain.9213@gmail.com>
2025-11-22 19:49:50 +0600

    .

--- main/build_tool/process.mjs
+++ main/build_tool/process.mjs
@@ -237,7 +237,7 @@ async function bundleAndWriteJs(filePath, content, scriptDir, type) {
             console.log(path.join(process.cwd(), 'config.mjs'));
             const configModule = await import(path.resolve(process.cwd(), 'config.mjs'));
             esbuildConfig = configModule.esbuildConfig || {};
-        } catch {
+        } catch(err) {
              console.warn('esbuildConfig not found, proceeding with default settings.',err);
         }
 

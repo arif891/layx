dd47ad492b6650ef9b1e0e3c89424032cdfb2ff4
Arif <arif.hossain.9213@gmail.com>
2025-12-26 23:52:47 +0600

    .

--- layx/others/syntax_highlighter/languages/css.js
+++ layx/others/syntax_highlighter/languages/css.js
@@ -1,90 +1,56 @@
 export default [
 	{
-		match: /\/\*((?!\*\/)[^])*(\*\/)?/g,
+		match: /\/\*[\s\S]*?\*\/|\/\/.*/g,
 		sub: 'todo'
 	},
 	{
 		expand: 'str'
 	},
 	{
+		match: /url\((?:(['"])(?:(?!\1).)*\1|[^)])*\)/g,
+		sub: [
+			{ type: 'func', match: /^url/g },
+			{ type: 'str', match: /[^()]+/g }
+		]
+	},
+	{
 		type: 'kwd',
-		match: /@[\w-]+\b|\b(and|not|only|or|from|to)\b/g
+		match: /@[\w-]+\b/g
+	},
+	{
+		type: 'type',
+		match: /!important\b/g
 	},
 	{
 		type: 'var',
-		match: /--[\w-]+/g
+	 match: /--[\w-]+|&/g
 	},
 	{
 		type: 'var',
-		match: /[\w-]+(?=\s*:(?!:))/g
-	},
-	{
-		type: 'var',
-		match: /&/g
+		match: /[\w-]+(?=\s*(?::|<=|>=|<|>|=))/g
 	},
 	{
 		type: 'num',
-		match: /#[\da-fA-F]{3,8}\b/gi
+		match: /#[\da-fA-F]{3,8}\b/g
 	},
 	{
 		type: 'num',
-		match: /-?(?:\d+\.?\d*|\.\d+)(cm|mm|in|px|pt|pc|em|ex|ch|rem|vw|vh|vmin|vmax|%|deg|rad|grad|turn|s|ms|fr|dpi|dpcm|dppx|q|lh|rlh|vi|vb|cqw|cqh|cqi|cqb|cqmin|cqmax|dvw|dvh|lvw|lvh|svw|svh)\b/gi
+		match: /-?(?:\d+\.?\d*|\.\d+)(?:(?:cm|mm|in|px|pt|pc|em|ex|ch|rem|vw|vh|vmin|vmax|deg|rad|grad|turn|s|ms|fr|dpi|dpcm|dppx|q|lh|rlh|vi|vb|cqw|cqh|cqi|cqb|cqmin|cqmax|dvw|dvh|lvw|lvh|svw|svh)\b|%(?!\w))/gi
 	},
 	{
 		type: 'num',
 		match: /-?(?:\d+\.?\d*|\.\d+)(?!\w)/g
 	},
 	{
-		type: 'kwd',
-		match: /(?<=^|[{};,\s&])(?:[a-z][\w-]*|#[\w-]+|\.[\w-]+|::?[\w-]+|\[[\w-]+[~|^$*]?=?[^\]]*\]|\*|>|\+|~)(?=\s*(?:[{,>+~[\s]|$))/gm
-	},
-	{
-		match: /url\([^)]*\)/g,
-		sub: [
-			{
-				type: 'func',
-				match: /url(?=\()/g
-			},
-			{
-				type: 'str',
-				match: /[^()]+/g
-			}
-		]
-	},
-	{
 		type: 'func',
-		match: /\b[a-zA-Z][\w-]*(?=\s*\()/g
+		match: /(?!(?:and|not|or|only)\b)[\w-]+\s*(?=\()/g
 	},
 	{
 		type: 'bool',
-		match: /\b(auto|inherit|initial|unset|revert|revert-layer|none|normal|all|both|transparent|currentColor)\b/g
+		match: /\b(auto|inherit|initial|unset|revert|revert-layer|none|normal|all|both|transparent|currentColor|true|false|block|inline|inline-block|flex|inline-flex|grid|inline-grid|subgrid|contents|table|inline-table|list-item|run-in|ruby|ruby-base|ruby-text|ruby-base-container|ruby-text-container|flexbox|inline-flexbox|absolute|relative|fixed|sticky|static|left|right|top|bottom|center|middle|start|end|flex-start|flex-end|space-between|space-around|space-evenly|baseline|stretch|bold|bolder|lighter|italic|oblique|underline|overline|line-through|uppercase|lowercase|capitalize|pre|pre-wrap|pre-line|nowrap|wrap|break-word|clip|ellipsis|solid|dashed|dotted|double|groove|ridge|inset|outset|hidden|and|not|only|or|from|to)\b/g
 	},
 	{
-		type: 'bool',
-		match: /\b(true|false)\b/g
-	},
-	{
-		type: 'bool',
-		match: /\b(block|inline|inline-block|flex|inline-flex|grid|inline-grid|subgrid|contents|table|inline-table|list-item|run-in|ruby|ruby-base|ruby-text|ruby-base-container|ruby-text-container|flexbox|inline-flexbox)\b/g
-	},
-	{
-		type: 'bool',
-		match: /\b(absolute|relative|fixed|sticky|static|fixed)\b/g
-	},
-	{
-		type: 'bool',
-		match: /\b(left|right|top|bottom|center|middle|start|end|flex-start|flex-end|space-between|space-around|space-evenly|baseline|stretch)\b/g
-	},
-	{
-		type: 'bool',
-		match: /\b(bold|bolder|lighter|italic|oblique|underline|overline|line-through|uppercase|lowercase|capitalize|pre|pre-wrap|pre-line|nowrap|wrap|break-word|clip|ellipsis)\b/g
-	},
-	{
-		type: 'bool',
-		match: /\b(white|black|red|green|blue|yellow|orange|purple|pink|gray|grey|brown|cyan|magenta|silver|gold)\b/g
-	},
-	{
-		type: 'bool',
-		match: /\b(solid|dashed|dotted|double|groove|ridge|inset|outset|hidden)\b/g
+		type: 'kwd',
+		match: /(?:\.|#|::?)[a-zA-Z0-9_\-]+|(?<!@[\w-]+\s+)(?<![\w-])[a-z][\w-]*(?![a-z0-9_\-])/gmi
 	}
 ]

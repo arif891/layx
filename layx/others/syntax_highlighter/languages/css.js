abd7c57597032ca0bf85498d8499efe285ec8d75
Arif <arif.hossain.9213@gmail.com>
2025-12-27 00:51:09 +0600

    Fix

--- layx/others/syntax_highlighter/languages/css.js
+++ layx/others/syntax_highlighter/languages/css.js
@@ -23,7 +23,7 @@ export default [
 	},
 	{
 		type: 'var',
-	 match: /--[\w-]+|&/g
+		match: /--[\w-]+/g
 	},
 	{
 		type: 'var',
@@ -43,14 +43,14 @@ export default [
 	},
 	{
 		type: 'func',
-		match: /(?!(?:and|not|or|only)\b)[\w-]+\s*(?=\()/g
+		match: /[\w-][\w-]*(?=\()/g
 	},
 	{
 		type: 'bool',
-		match: /\b(auto|inherit|initial|unset|revert|revert-layer|none|normal|all|both|transparent|currentColor|true|false|block|inline|inline-block|flex|inline-flex|grid|inline-grid|subgrid|contents|table|inline-table|list-item|run-in|ruby|ruby-base|ruby-text|ruby-base-container|ruby-text-container|flexbox|inline-flexbox|absolute|relative|fixed|sticky|static|left|right|top|bottom|center|middle|start|end|flex-start|flex-end|space-between|space-around|space-evenly|baseline|stretch|bold|bolder|lighter|italic|oblique|underline|overline|line-through|uppercase|lowercase|capitalize|pre|pre-wrap|pre-line|nowrap|wrap|break-word|clip|ellipsis|solid|dashed|dotted|double|groove|ridge|inset|outset|hidden|and|not|only|or|from|to)\b/g
+		match: /\b(auto|inherit|initial|unset|revert|revert-layer|none|normal|all|both|transparent|currentColor|true|false|block|inline|flex|grid|subgrid|contents|table|list-item|run-in|ruby|flexbox|absolute|relative|fixed|sticky|static|left|right|top|bottom|center|middle|start|end|space-between|space-around|space-evenly|baseline|stretch|bold|bolder|lighter|italic|oblique|underline|overline|line-through|uppercase|lowercase|capitalize|pre|nowrap|wrap|break-word|clip|ellipsis|solid|dashed|dotted|double|groove|ridge|inset|outset|hidden|and|not|only|or|from|to)(?:-[\w-]+)?\b/g
 	},
 	{
 		type: 'kwd',
-		match: /(?:\.|#|::?)[a-zA-Z0-9_\-]+|(?<!@[\w-]+\s+)(?<![\w-])[a-z][\w-]*(?![a-z0-9_\-])/gmi
+		match: /(?:\.|#|::?)[a-zA-Z0-9_\-]+|(?<!@[\w-]+\s+(?:[\w-]+\s*,\s*)*)(?<![\w-])[a-z][\w-]*(?![a-z0-9_\-])/gmi
 	}
 ]

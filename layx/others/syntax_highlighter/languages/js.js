d6a74e9ba75547fe515945cad43e3741e4269067
Arif <arif.hossain.9213@gmail.com>
2025-12-28 20:59:58 +0600

    fix

--- layx/others/syntax_highlighter/languages/js.js
+++ layx/others/syntax_highlighter/languages/js.js
@@ -20,42 +20,40 @@ export default [
 		match: new class {
 			exec(str) {
 				let i = this.lastIndex;
-				for (; i < str.length - 2; ++i) {
-					if (str[i] === '$' && str[i + 1] === '{' && str[i - 1] !== '\\') {
-						let start = i, depth = 1, j = i + 2;
-						let inString = null;
-
-						while (j < str.length && depth > 0) {
-							const char = str[j];
+				for (; i < str.length; ++i) {
+					if (str[i] === '`' && str[i - 1] !== '\\') {
+						let start = i, j = i + 1, depth = 0, inString = null;
+						while (j < str.length) {
+							const char = str[j], next = str[j + 1];
 							if (char === '\\') { j += 2; continue; }
-
-							if (!inString && (char === "'" || char === '"' || char === '`' || char === '/')) {
-								inString = char;
-							} else if (inString === char) {
-								inString = null;
+							if (depth === 0) {
+								if (char === '`') {
+									this.lastIndex = j + 1;
+									return { index: start, 0: str.slice(start, j + 1) };
+								}
+								if (char === '$' && next === '{') { depth = 1; j++; }
+							} else {
+								if (!inString) {
+									if (char === '/' && next === '/') { inString = '//'; j++; }
+									else if (char === '/' && next === '*') { inString = '/*'; j++; }
+									else if (char === "'" || char === '"' || char === '`') inString = char;
+									else if (char === '{') depth++;
+									else if (char === '}') depth--;
+								} else {
+									if (inString === '//' && char === '\n') inString = null;
+									else if (inString === '/*' && char === '*' && next === '/') { inString = null; j++; }
+									else if (inString === char) inString = null;
+								}
 							}
-
-							if (!inString) {
-								if (char === '{') depth++;
-								if (char === '}') depth--;
-							}
-
-							if (depth > 0) j++;
+							j++;
 						}
-						this.lastIndex = j + 1;
-						return { index: start, 0: str.slice(start, j + 1) };
+						this.lastIndex = str.length;
+						return { index: start, 0: str.slice(start) };
 					}
 				}
 				return null;
 			}
 		}(),
-		sub: [
-			{ type: 'kwd', match: /^\${|}$/g },
-			{ match: /(?!^\$|{)[^]+(?=}$)/g, sub: 'js' },
-		],
-	},
-	{
-		match: /`([^\\`]|\\.)*`?/g,
 		sub: 'js_template_literals'
 	},
 	{
--- layx/others/syntax_highlighter/languages/js_template_literals.js
+++ layx/others/syntax_highlighter/languages/js_template_literals.js
@@ -7,26 +7,25 @@ export default [
 					if (str[i] === '$' && str[i + 1] === '{' && str[i - 1] !== '\\') {
 						let start = i, depth = 1, j = i + 2;
 						let inString = null;
-
 						while (j < str.length && depth > 0) {
-							const char = str[j];
+							const char = str[j], next = str[j + 1];
 							if (char === '\\') { j += 2; continue; }
-
-							if (!inString && (char === "'" || char === '"' || char === '`' || char === '/')) {
-								inString = char;
-							} else if (inString === char) {
-								inString = null;
-							}
-
 							if (!inString) {
-								if (char === '{') depth++;
-								if (char === '}') depth--;
+								if (char === '/' && next === '/') { inString = '//'; j++; }
+								else if (char === '/' && next === '*') { inString = '/*'; j++; }
+								else if (char === "'" || char === '"' || char === '`') inString = char;
+								else if (char === '{') depth++;
+								else if (char === '}') depth--;
+							} else {
+								if (inString === '//' && char === '\n') inString = null;
+								else if (inString === '/*' && char === '*' && next === '/') { inString = null; j++; }
+								else if (inString === char) inString = null;
 							}
-
 							if (depth > 0) j++;
 						}
-						this.lastIndex = j + 1;
-						return { index: start, 0: str.slice(start, j + 1) };
+						if (depth === 0) j++;
+						this.lastIndex = j;
+						return { index: start, 0: str.slice(start, j) };
 					}
 				}
 				return null;

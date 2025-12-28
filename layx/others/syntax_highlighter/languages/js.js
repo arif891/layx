76d5017f17778480daf48218ed4a0b01ad5eb4ca
Arif <arif.hossain.9213@gmail.com>
2025-12-28 19:18:00 +0600

    .

--- layx/others/syntax_highlighter/languages/js.js
+++ layx/others/syntax_highlighter/languages/js.js
@@ -17,31 +17,6 @@ export default [
     sub: 'regex'
   },
   {
-    match: new class {
-      exec(str) {
-        let i = this.lastIndex;
-        for (; i < str.length - 2; ++i) {
-          if (str[i] === '$' && str[i + 1] === '{' && str[i - 1] !== '\\') {
-            let start = i, depth = 1, j = i + 2;
-            while (j < str.length && depth > 0) {
-              if (str[j] === '\\') { j += 2; continue; }
-              if (str[j] === '{') depth++;
-              if (str[j] === '}') depth--;
-              if (depth > 0) j++;
-            }
-            this.lastIndex = j + 1;
-            return { index: start, 0: str.slice(start, j + 1) };
-          }
-        }
-        return null;
-      }
-    }(),
-    sub: [
-      { type: 'kwd', match: /^\${|}$/g },
-      { match: /(?!^\$|{)[^]+(?=}$)/g, sub: 'js' },
-    ],
-  },
-  {
     match: /`([^\\`]|\\.)*`?/g,
     sub: 'js_template_literals'
   },
--- layx/others/syntax_highlighter/languages/js_template_literals.js
+++ layx/others/syntax_highlighter/languages/js_template_literals.js
@@ -1,45 +1,27 @@
 export default [
 	{
 		match: new class {
-    exec(str) {
-        let i = this.lastIndex;
-        if (i >= str.length) return null;
-
-        // Look for the start of an interpolation: ${
-        for (; i < str.length - 2; ++i) {
-            // Check for ${ but ensure it's not escaped by a backslash
-            if (str[i] === '$' && str[i + 1] === '{' && str[i - 1] !== '\\') {
-                let start = i;
-                let depth = 1;
-                let j = i + 2;
-
-                // Move forward until we find the matching closing brace }
-                while (j < str.length && depth > 0) {
-                    if (str[j] === '\\') {
-                        j += 2; // Skip escaped characters
-                        continue;
-                    }
-                    if (str[j] === '{') depth++;
-                    if (str[j] === '}') depth--;
-                    if (depth > 0) j++;
-                }
-
-                this.lastIndex = j + 1;
-                return { index: start, 0: str.slice(start, j + 1) };
-            }
-        }
-        return null;
-    }
-}(),
+			exec(str) {
+				let i = this.lastIndex;
+				for (; i < str.length - 2; ++i) {
+					if (str[i] === '$' && str[i + 1] === '{' && str[i - 1] !== '\\') {
+						let start = i, depth = 1, j = i + 2;
+						while (j < str.length && depth > 0) {
+							if (str[j] === '\\') { j += 2; continue; }
+							if (str[j] === '{') depth++;
+							if (str[j] === '}') depth--;
+							if (depth > 0) j++;
+						}
+						this.lastIndex = j + 1;
+						return { index: start, 0: str.slice(start, j + 1) };
+					}
+				}
+				return null;
+			}
+		}(),
 		sub: [
-			{
-				type: 'kwd',
-				match: /^\${|}$/g
-			},
-			{
-				match: /(?!^\$|{)[^]+(?=}$)/g,
-				sub: 'js'
-			},
+			{ type: 'kwd', match: /^\${|}$/g },
+			{ match: /(?!^\$|{)[^]+(?=}$)/g, sub: 'js' },
 		],
 	},
 ];
